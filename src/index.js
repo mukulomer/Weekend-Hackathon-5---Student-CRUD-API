82c5857a679e7767223c83a9eb903e8724ad88cf
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-11-20 17:12:28 +0530

    final

--- src/index.js
+++ src/index.js
@@ -35,58 +35,110 @@ app.get('/api/student/:id',(req,res)=>{
    
 });
 
-
-app.post('/api/student',(req,res)=>{
-   
-    const schema ={
-        id: ID+1,
-        name: req.body.name,
-        currentClass: req.body.currentClass,
-        division: req.body.division
-    };
-   
-    if(!schema.name|| !schema.currentClass || !schema.division)
-    {
-        res.status(400).send("Incomplete data");
-        return;
-    }
-  
-    student.push(schema);
-     ID++;
-    res.send({id: ID});
-
-});
-
-
-
-app.put('/api/student/:id',(req,res)=>{
-
-   const id = student.find(Student=> Student.id === parseInt(req.params.id));
-
-   if(!id)
-   {
-    res.status(400).send(`Not Found! ${id}`); 
-      return;   
-   }
-
-   const schema = {
-    id: parseInt(req.params.id),
-    name: req.body.name,
-    currentClass: req.body.currentClass,
-    division: req.body.division
-   };
-  
-  
-if(!schema.name|| !schema.currentClass || !schema.division)
-    {
-        res.status(400).send("Incomplete data");
-        return;
-    }
+app.post("/api/student", (req, res) => {
+        const newStudent = req.body;
+        if(!newStudent.name || !newStudent.currentClass || !newStudent.division){
+            res.sendStatus(400);
+            return;
+        }
     
-    student.splice(parseInt(req.params.id), 1,schema);
-    res.type('application/x-www-form-urlencoded').send(student);
+        student.push({
+            id: ID+1,
+            name: newStudent.name,
+            currentClass: parseInt(newStudent.currentClass),
+            division: newStudent.division
+        });
+    
+        ID++;
+        
+        res.send({
+            ID: ID
+        });
+    });
+    
+    app.put("/api/student/:id", (req, res) => {
+        const id = parseInt(req.params.id);
+        if(isNaN(id)){
+            res.sendStatus(400);
+            return;
+        }
+        
+        const index = student.findIndex(student => student.id === id);
+        
+        if(index === -1){
+            res.sendStatus(400);
+            return;
+        }
+        
+        // const student = student[index];
+        if(req.body.name){
+            student[index].name=req.body.name;
+        }
+        if(req.body.currentClass){
+            student[index].currentClass=parseInt(req.body.currentClass);
+        }
+        if(req.body.division){
+            student[index].division=req.body.division;
+        }
+        res.set("content-type", "application/x-www-form-urlencoded");
+    
+        res.send(req.body.name);
+        
+        
+    });
 
-});
+
+// app.post('/api/student',(req,res)=>{
+   
+//     const schema ={
+//         id: ID+1,
+//         name: req.body.name,
+//         currentClass: req.body.currentClass,
+//         division: req.body.division
+//     };
+   
+//     if(!schema.name|| !schema.currentClass || !schema.division)
+//     {
+//         res.status(400).send("Incomplete data");
+//         return;
+//     }
+  
+//     student.push(schema);
+//      ID++;
+//     res.send({id: ID});
+
+// });
+
+
+
+// app.put('/api/student/:id',(req,res)=>{
+
+//    const id = student.find(Student=> Student.id === parseInt(req.params.id));
+
+//    if(!id)
+//    {
+//     res.status(400).send(`Not Found! ${id}`); 
+//       return;   
+//    }
+
+//    const schema = {
+//     id: parseInt(req.params.id),
+//     name: req.body.name,
+//     currentClass: req.body.currentClass,
+//     division: req.body.division
+//    };
+  
+  
+// if(!schema.name|| !schema.currentClass || !schema.division)
+//     {
+//         res.status(400).send("Incomplete data");
+//         return;
+//     }
+    
+//     student.splice(parseInt(req.params.id), 1,schema);
+//     res.type('application/x-www-form-urlencoded').send(student);
+
+// });
 
 app.delete('/api/student/:id',(req,res)=>{
     const id = student.find(Student=> Student.id === parseInt(req.params.id));
